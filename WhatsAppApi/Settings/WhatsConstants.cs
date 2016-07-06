b160c763543d0547fa0f1a20edad88dad6c42771
NirShalmon <nir.shalmon00@gmail.com>
2016-07-06 15:31:13 +0300

    Update WhatsConstants.cs

--- WhatsAppApi/Settings/WhatsConstants.cs
+++ WhatsAppApi/Settings/WhatsConstants.cs
@@ -38,8 +38,9 @@ namespace WhatsAppApi.Settings
         /// </summary>
 		//public const string WhatsAppVer = "2.13.21";
         //public const string WhatsAppVer = "2.12.440";
-        public const string WhatsAppVer = "2.12.556";
-
+        //public const string WhatsAppVer = "2.12.556";
+	public const string WhatsAppVer = "2.16.148";
+	
         /// <summary>
         /// The port that needs to be connected to
         /// </summary>
@@ -66,7 +67,7 @@ namespace WhatsAppApi.Settings
         /// </summary>
 		//public const string UserAgent = "WhatsApp/2.13.21 S40Version/14.26 Device/Nokia302";
         //public const string UserAgent = "WhatsApp/2.12.440 Android/4.3 Device/Xiaomi-HM_1SW";
-        public const string UserAgent = "WhatsApp/2.12.556 Android/4.3 Device/Xiaomi-HM_1SW";
+        public const string UserAgent = "WhatsApp/2.16.148 Android/4.3 Device/Xiaomi-HM_1SW";
 
 
         #endregion
